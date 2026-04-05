268badcf04574a655699f31c99a005fe0f4b5af7
marvelouscodes <marvelouspeter90@gmail.com>
2026-04-05 19:01:42 +0100

    fix class

--- database/seeders/TopicsSeeder.php
+++ database/seeders/TopicsSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class TopicsLitHistAccSeeder extends Seeder
+class TopicsSeeder extends Seeder
 {
     public function run(): void
     {
